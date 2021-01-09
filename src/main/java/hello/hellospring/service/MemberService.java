d7f2c4689673f61247b6c5e508ec7acc62e8356c
email <crosea@naver.com>
2021-01-09 21:43:45 +0900

    ~ 회원 서비스 테스트

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,38 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memoryMemberRepository;
+    public MemberService(MemberRepository memoryMemberRepository) { this.memoryMemberRepository = memoryMemberRepository;
+    }
+
+    public Long join (Member member){
+
+        duplicate(member);
+        memoryMemberRepository.save(member);
+        return member.getId();
+    }
+
+    private void duplicate (Member member){
+
+        memoryMemberRepository.findByName(member.getName()).ifPresent(m->{throw new IllegalStateException("이미 존재하는 회원입니당.");});
+    }
+
+    public List<Member> findMembers (){
+
+        return memoryMemberRepository.findAll();
+    }
+
+    public Optional<Member> findOne (Long memberId){
+
+        return memoryMemberRepository.findById(memberId);
+    }
+
+}
